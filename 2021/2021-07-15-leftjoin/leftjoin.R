a7c12d53e6550fe6d11f7b40818703fb941af1dc
Santhiago Cristiano <santhiagocristiano96@gmail.com>
2021-07-16 01:56:10 -0300

    pequenas modificações

--- 2021/2021-07-15-leftjoin/leftjoin.R
+++ 2021/2021-07-15-leftjoin/leftjoin.R
@@ -47,35 +47,43 @@ auxiliar <- abjData::muni %>%
 
 # left join ---------------------------------------------------------------
 
-acrescenta_cod_uf_reg <- function(df) {
-  df %>%
+mesclar_dados <- function(x) {
+  x %>%
     left_join(auxiliar, by = c("codigo" = "muni_id_6")) %>%
     select(-codigo) %>%
     rename(cod_ibge = muni_id, uf = uf_sigla, regiao = regiao_nm) %>%
     relocate(c(cod_ibge, regiao, uf), .before = municipio)
 }
 
-morte_infantil_final <- map(morte_infantil, acrescenta_cod_uf_reg)
+morte_infantil_mesclado <- map(morte_infantil, mesclar_dados)
 
 
 # exportando os dados -----------------------------------------------------
 
-morte_infantil_final %>%
+# utilizando o pipe
+
+morte_infantil_mesclado %>%
   names(.) %>%
   walk( ~ write.csv2(
-    morte_infantil_final[[.]],
-    glue("./2021/2021-07-15-leftjoin/dados-modificados/{.}-modificado.csv"),
+    morte_infantil_mesclado[[.]],
+    file = glue("./2021/2021-07-15-leftjoin/dados-mesclados/{.}-mesclado.csv"),
     row.names = FALSE
   ))
 
-# ou
+# sem o pipe
 
 walk(
-  names(morte_infantil_final),
+  names(morte_infantil_mesclado),
   ~ write.csv2(
-    morte_infantil_final[[.]],
-    glue("./2021/2021-07-15-leftjoin/dados-modificados/{.}-modificado.csv"),
+    morte_infantil_mesclado[[.]],
+    file = glue("./2021/2021-07-15-leftjoin/dados-mesclados/{.}-mesclado.csv"),
     row.names = FALSE
   )
 )
+
+# todos os anos empilhados
+
+map_df(morte_infantil, mesclar_dados) %>%
+  write.csv2(file = "./2021/2021-07-15-leftjoin/dados-mesclados/mortes_infantis_mesclado.csv",
+             row.names = FALSE)
 
